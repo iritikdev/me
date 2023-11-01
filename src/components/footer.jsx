d9d882f77554890ec77f5c73b9e676f71cee0007
Ritik <iritikdev@gmail.com>
2023-11-01 23:32:08 +0530

    Reimplement Footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,74 +1,31 @@
-import { Box, Typography } from "@mui/material";
-import React from "react";
-
-import { bc } from "../assets";
-import { theme } from "../theme";
-import SocialIcons from "./SocialIcons";
+import { Box, Container, Typography } from "@mui/material";
+import { AiOutlineCopyrightCircle } from 'react-icons/ai'
+import { IoConstructOutline } from 'react-icons/io5'
+import SocialIcons from './SocialIcons';
+import { FiGithub, FiLinkedin, FiTwitter } from "react-icons/fi";
 
 function Footer(props) {
   return (
     <Box
       sx={{
-        height: 70,
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-        rowGap: 1.1,
-        mb: {
-          xs: 3,
-          md: 0,
-          lg: 0,
-        },
+        backdropFilter: "blur(16px) saturate(180%)",
+        backgroundColor: "rgba(17, 25, 40, 0.75)",
+        borderTop: "1px solid rgba(255, 255, 255, 0.125)",
+        py: 5,
       }}
     >
-      <Box>
-        <Typography
-          sx={{
-            display: "inline-flex",
-            alignItems: "center",
-            textAlign: "center",
-            cursor: "pointer",
-            pb: 1,
-            lineHeight:1.5,
-            fontSize: 13,
-            fontWeight: "300",
-            fontFamily: "Fira Code",
-            color: theme.palette.slate[200],
-            "&:hover": {
-              color: theme.palette.green.main,
-            },
-          }}
-        >
-         © {new Date().getFullYear()}, Ritik Kumar. All rights reserved.
-         <br />
-         This site has been developed by myself using React, MUI.
-          
-          <Typography
-            component={"span"}
-            sx={{
-              fontSize: 12,
-              fontFamily: "Inter",
-              fontWeight: "600",
-            }}
-          ></Typography>
-        </Typography>
-      </Box>
-
-      <Box
-        className="social-icons"
-        sx={{
-          display: {
-            xs: "flex",
-            sm: "none",
-            md: "none",
-            lg: "none",
-          },
-          columnGap: 2,
-        }}
-      >
-        <SocialIcons direction="row" />
-      </Box>
+      <Container sx={{display:"flex", justifyContent: "space-between", flexDirection:{xs: "column", sm: "row"}, alignItems:"center", gap:2 }}>
+        <Typography color={"white"} display="flex" alignItems="center" gap={1}><AiOutlineCopyrightCircle /> Ritik Sharma</Typography>
+        <Box display={"flex"} gap={2} >
+        <FiGithub size={20} color="#ccd6f6" strokeWidth={2} />
+        <FiLinkedin size={20} color="#ccd6f6" strokeWidth={2} />
+        <FiTwitter size={20} color="#ccd6f6" strokeWidth={2} />
+        </Box>
+        <Box display={"flex"} gap={2}>
+          <Typography color={"white"}>Terms of Use</Typography>
+          <Typography color={"white"}>Privacy Policy</Typography>
+        </Box>
+      </Container>
     </Box>
   );
 }
